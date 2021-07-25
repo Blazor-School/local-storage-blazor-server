5426f1f5d38694247710d86bc60a7f93e47473ae
Red Wei <hevilhuy2@gmail.com>
2021-07-25 09:52:26 +0700

    Added example for local storage.

---
+++ LocalStorageBlazorServerExample/Data/LocalStorageService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Threading.Tasks;
+
+namespace LocalStorageBlazorServerExample.Data
+{
+    public class LocalStorageService
+    {
+        private readonly ProtectedLocalStorage _protectedLocalStorage;
+
+        public LocalStorageService(ProtectedLocalStorage protectedLocalStorage)
+        {
+            _protectedLocalStorage = protectedLocalStorage;
+        }
+
+        public async Task StoreValueAsync(string key, string value)
+        {
+            await _protectedLocalStorage.SetAsync("login", key, value);
+        }
+    }
+}
--- LocalStorageBlazorServerExample/Startup.cs
+++ LocalStorageBlazorServerExample/Startup.cs
@@ -1,14 +1,10 @@
 using LocalStorageBlazorServerExample.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LocalStorageBlazorServerExample
 {
@@ -27,7 +23,7 @@ namespace LocalStorageBlazorServerExample
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<LocalStorageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
